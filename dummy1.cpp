f072d939a835f5cef6c2985f9e969f0bb25d9e85
Adrian Hernando <ahsolanas@gmail.com>
2019-05-16 13:24:26 +0200

    Create dummy1.cpp

---
+++ dummy1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main() 
+{
+  int varInt = 123456;
+  string varString = "DefaultString";
+  char arrChar[128] = "Long char array right there ->";
+  int * ptr2int;
+  ptr2int = &varInt;
+  int ** ptr2ptr;
+  ptr2ptr = &ptr2int;
+  int ** ptr2ptr2;
+  
+  While (1){
+    cout << "Process ID: " << GetCurrentProcessId() << endl;
+    
+    cout << "varInt (0x" << &varInt << ") = " << varInt << endl;
+    cout << "varString (0x" << &varString << ") = " << varString << endl;
+    cout << "varChar (0x" << &varChar << ") = " << varChar << endl;
+    
+    cout << "ptr2int (0x" << hex << &ptr2int << ") = " << ptr2int << endl;
+    cout << "ptr2ptr (0x" << hex << &ptr2ptr << ") = " << ptr2ptr << endl;
+    cout << "ptr2ptr2 (0x" << hex << &ptr2ptr2 << ") = " << ptr2ptr2 << endl;
+    
+    cout << "Press ENTER to print again." << endl;
+    getchar();
+    cout << "-----------------------------------" << endl;
+  }
+
+  return 0;
+}
