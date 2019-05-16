4387c951791cf2d19620fbce2a982a5f4f18aa65
Adrian Hernando <ahsolanas@gmail.com>
2019-05-16 15:49:17 +0200

    Update Dummy Target 1.cpp

--- Dummy Target 1/Dummy Target 1.cpp
+++ Dummy Target 1/Dummy Target 1.cpp
@@ -1,21 +1,23 @@
 #include "pch.h"
 #include <iostream>
 #include <Windows.h>
+#include <string>
 using namespace std;
+#define CHAR_ARRAY_SIZE 128
 
 int main()
 {
 	int varInt = 123456;
-	char varString[] = "DefaultString";
-	char arrChar[128] = "Long char array right there ->";
+	string varString = "DefaultString";
+	char arrChar[CHAR_ARRAY_SIZE] = "Long char array right there ->";
 	int * ptr2int;
 	ptr2int = &varInt;
 	int ** ptr2ptr;
 	ptr2ptr = &ptr2int;
-	int ** ptr2ptr2;
-	ptr2ptr2 = &ptr2ptr
+	int *** ptr2ptr2;
+	ptr2ptr2 = &ptr2ptr;
 
-	while(1) {
+	while(True) {
 		cout << "Process ID: " << GetCurrentProcessId() << endl;
 
 		cout << "varInt (0x" << &varInt << ") = " << varInt << endl;
